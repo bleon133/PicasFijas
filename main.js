7ed50dc170075ec1d14a3aa532321d1b0277155c
BrayanLeon <brot10102017@gmail.com>
2023-02-22 15:34:00 -0500

    version1

---
+++ main.js
@@ -0,0 +1,19 @@
+//Entradas
+const number1 = document.getElementById('num1')
+const number2 = document.getElementById('num2')
+const number3 = document.getElementById('num3')
+const number4 = document.getElementById('num4')
+//Boton
+const calcular = document.getElementById('calcular')
+//Salidas
+const status = document.getElementById('marcador')
+const historial = document.getElementById('results')
+
+calcular.addEventListener('click',()=>{
+    console.log(num)
+    /* if((number1 > 9 || number1 < 0) || (number2 > 9 || number2 < 0) || (number3 > 9 || number3 < 0) || (number4 > 9 || number4 < 0)){
+        alert('Error: Los 4 números no pueden ser mayor a 9 ni menor a 0.')
+    }else{
+        console.log(Number(number1))
+    } */
+})
